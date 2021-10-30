5691595830d9a55a39fa9bede016949d0083ec89
hayesall <alexander@batflyer.net>
2021-10-30 15:17:54 -0400

    🎨 Formatting and import removal in `AUCCalculator`

--- src/main/java/com/goadrich/mark/auc/AUCCalculator.java
+++ src/main/java/com/goadrich/mark/auc/AUCCalculator.java
@@ -1,42 +1,27 @@
-/*     */ package com.goadrich.mark.auc;
-/*     */
-/*     */ import java.io.BufferedReader;
-/*     */ import java.io.File;
-/*     */ import java.io.FileNotFoundException;
-/*     */ import java.io.FileReader;
-/*     */ import java.io.IOException;
-/*     */ import java.util.NoSuchElementException;
-/*     */ import java.util.StringTokenizer;
-/*     */
-/*     */
-/*     */
-/*     */
-/*     */
-/*     */
-/*     */ public class AUCCalculator
-/*     */ {
-/*     */   private static String fileName;
-/*     */   private static String fileType;
-/*     */   private static double posCount;
-/*     */   private static double negCount;
-/*  22 */   private static double minRecall = 0.0D;
-/*     */
-/*     */
-/*     */   public static boolean DEBUG = false;
-/*     */
-/*     */   public static void main(String[] paramArrayOfString) {
+package com.goadrich.mark.auc;
 
-              // TODO(hayesall): My unsafe refactor introduced nulls
-              fileName = paramArrayOfString[0];
-              fileType = paramArrayOfString[1];
-              minRecall = Double.parseDouble(paramArrayOfString[2]);
+public class AUCCalculator
+{
+  private static String fileName;
+  private static String fileType;
+  private static double posCount;
+  private static double negCount;
+  private static double minRecall = 0.0;
 
-              assert minRecall >= 0.0D;
-              assert minRecall <= 1.0D;
+  public static boolean DEBUG = false;
 
-              Confusion confusion = ReadList.readFile(fileName, fileType);
-/*  40 */     // confusion.writeROCFile(fileName + ".roc");
-/*  41 */     confusion.calculateAUCPR(minRecall);
-/*  42 */     confusion.calculateAUCROC();
-/*     */   }
-/*     */ }
+  public static void main(String[] paramArrayOfString)
+  {
+    // TODO(hayesall): My unsafe refactor introduced nulls
+    fileName = paramArrayOfString[0];
+    fileType = paramArrayOfString[1];
+    minRecall = Double.parseDouble(paramArrayOfString[2]);
+
+    assert minRecall >= 0.0;
+    assert minRecall <= 1.0;
+
+    Confusion confusion = ReadList.readFile(fileName, fileType);
+    confusion.calculateAUCPR(minRecall);
+    confusion.calculateAUCROC();
+  }
+}
