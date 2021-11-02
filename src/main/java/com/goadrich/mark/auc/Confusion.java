1caf7cda6443e9e6fd466b9360174beb4f12a73d
hayesall <alexander@batflyer.net>
2021-11-02 11:26:25 -0400

    🎨 Drop decompiler comments in `Confusion.java`

--- src/main/java/com/goadrich/mark/auc/Confusion.java
+++ src/main/java/com/goadrich/mark/auc/Confusion.java
@@ -1,259 +1,185 @@
-/*     */ package com.goadrich.mark.auc;
-/*     */
-/*     */ import java.util.Arrays;
-/*     */ import java.util.Vector;
-/*     */
-/*     */
-/*     */
-/*     */
-/*     */
-/*     */
-/*     */
-/*     */ public class Confusion
-/*     */   extends Vector<PNPoint>
-/*     */ {
-/*     */   private final double totPos;
-/*     */   private final double totNeg;
-/*     */
-/*     */   public Confusion(double paramDouble1, double paramDouble2) {
-/*  23 */     if (paramDouble1 < 1.0D || paramDouble2 < 1.0D) {
-/*  24 */       this.totPos = 1.0D;
-/*  25 */       this.totNeg = 1.0D;
-/*  26 */       System.err.println("ERROR: " + paramDouble1 + "," + paramDouble2 + " - " + "Defaulting Confusion to 1,1");
-/*     */     } else {
-/*     */
-/*  29 */       this.totPos = paramDouble1;
-/*  30 */       this.totNeg = paramDouble2;
-/*     */     }
-/*     */   }
-/*     */
-/*     */
-/*     */
-/*     */
-/*     */
-/*     */
-/*     */
-/*     */
-/*     */
-/*     */
-/*     */
-/*     */   public void addPoint(double paramDouble1, double paramDouble2) throws NumberFormatException {
-/*  89 */     if (paramDouble1 < 0.0D || paramDouble1 > this.totPos || paramDouble2 < 0.0D || paramDouble2 > this.totNeg) {
-/*  90 */       throw new NumberFormatException();
-/*     */     }
-/*     */
-/*  93 */     PNPoint pNPoint = new PNPoint(paramDouble1, paramDouble2);
-/*  94 */     if (!contains(pNPoint)) {
-/*  95 */       add(pNPoint);
-/*     */     }
-/*     */   }
-/*     */
-/*     */
-/*     */
-/*     */
-/*     */
-/*     */   public void sort() {
-/*     */
-/*     */
-/* 109 */     if (size() == 0) {
-/* 110 */       System.err.println("ERROR: No data to sort....");
-/*     */
-/*     */       return;
-/*     */     }
-/*     */
-/* 115 */     PNPoint[] arrayOfPNPoint = new PNPoint[size()];
-/* 116 */     byte b1 = 0;
-/* 117 */     while (size() > 0) {
-/* 118 */       arrayOfPNPoint[b1++] = elementAt(0);
-/* 119 */       removeElementAt(0);
-/*     */     }
-/* 121 */     Arrays.sort(arrayOfPNPoint);
+package com.goadrich.mark.auc;
 
-              this.addAll(Arrays.asList(arrayOfPNPoint));
+import java.util.Arrays;
+import java.util.Vector;
 
-/*     */
-/*     */
-/* 127 */     PNPoint pNPoint1 = elementAt(0);
-/* 128 */     while (pNPoint1.getPos() < 0.001D && pNPoint1.getPos() > -0.001D) {
-/* 129 */       removeElementAt(0);
-/* 130 */       pNPoint1 = elementAt(0);
-/*     */     }
-/*     */
-/*     */
-/* 134 */     double d = pNPoint1.getNeg() / pNPoint1.getPos();
-/*     */
-/* 136 */     PNPoint pNPoint2 = new PNPoint(1.0D, d);
-/* 137 */     if (!contains(pNPoint2) && pNPoint1.getPos() > 1.0D) {
-/* 138 */       insertElementAt(pNPoint2, 0);
-/*     */     }
-/*     */
-/*     */
-/* 142 */     pNPoint2 = new PNPoint(this.totPos, this.totNeg);
-/* 143 */     if (!contains(pNPoint2)) {
-/* 144 */       add(pNPoint2);
-/*     */     }
-/*     */   }
-/*     */
-/*     */
-/*     */
-/*     */
-/*     */
-/*     */
-/*     */   public void interpolate() {
-/*     */
-/*     */
-/* 159 */     if (size() == 0) {
-/* 160 */       System.err.println("ERROR: No data to interpolate....");
-/*     */
-/*     */       return;
-/*     */     }
-/*     */
-/* 165 */     for (byte b = 0; b < size() - 1; b++) {
-/*     */
-/*     */
-/* 168 */       PNPoint pNPoint1 = elementAt(b);
-/* 169 */       PNPoint pNPoint2 = elementAt(b + 1);
-/*     */
-/*     */
-/* 172 */       double d1 = pNPoint2.getPos() - pNPoint1.getPos();
-/* 173 */       double d2 = pNPoint2.getNeg() - pNPoint1.getNeg();
-/* 174 */       double d3 = d2 / d1;
-/* 175 */       double d4 = pNPoint1.getPos();
-/* 176 */       double d5 = pNPoint1.getNeg();
-/*     */
-/*     */
-/* 179 */       while (Math.abs(pNPoint1.getPos() - pNPoint2.getPos()) > 1.001D) {
-/*     */
-/* 181 */         double d = d5 + (pNPoint1.getPos() - d4 + 1.0D) * d3;
-/*     */
-/* 183 */         PNPoint pNPoint = new PNPoint(pNPoint1.getPos() + 1.0D, d);
-/*     */
-/*     */
-/* 186 */         insertElementAt(pNPoint, ++b);
-/* 187 */         pNPoint1 = pNPoint;
-/*     */       }
-/*     */     }
-/*     */   }
-/*     */
-/*     */
-/*     */
-/*     */
-/*     */
-/*     */
-/*     */
-/*     */   public double calculateAUCPR(double paramDouble) {
-/*     */
-/*     */
-/* 204 */     if (paramDouble < 0.0D || paramDouble > 1.0D) {
-/* 205 */       System.err.println("ERROR: invalid minRecall, must be between 0 and 1 - returning 0");
-/*     */
-/* 207 */       return 0.0D;
-/*     */     }
-/* 209 */     if (size() == 0) {
-/* 210 */       System.err.println("ERROR: No data to calculate....");
-/* 211 */       return 0.0D;
-/*     */     }
-/*     */
-/*     */
-/* 215 */     double d1 = paramDouble * this.totPos;
-/* 216 */     byte b = 0;
-/* 217 */     PNPoint pNPoint1 = elementAt(b);
-/* 218 */     PNPoint pNPoint2 = null;
-/*     */     try {
-/* 220 */       while (pNPoint1.getPos() < d1) {
-/* 221 */         pNPoint2 = pNPoint1;
-/* 222 */         pNPoint1 = elementAt(++b);
-/*     */       }
-/* 224 */     } catch (ArrayIndexOutOfBoundsException arrayIndexOutOfBoundsException) {
-/* 225 */       System.out.println("ERROR: minRecall out of bounds - exiting...");
-/* 226 */       System.exit(-1);
-/*     */     }
-/*     */
-/*     */
-/* 230 */     double d2 = (pNPoint1.getPos() - d1) / this.totPos;
-/* 231 */     double d3 = pNPoint1.getPos() / (pNPoint1.getPos() + pNPoint1.getNeg());
-/* 232 */     double d4 = d2 * d3;
-/*     */
-/* 234 */     if (pNPoint2 != null) {
-/* 235 */       double d5 = pNPoint1.getPos() / this.totPos - pNPoint2.getPos() / this.totPos;
-/*     */
-/* 237 */       double d6 = pNPoint1.getPos() / (pNPoint1.getPos() + pNPoint1.getNeg()) - pNPoint2.getPos() / (pNPoint2.getPos() + pNPoint2.getNeg());
-/*     */
-/* 239 */       double d7 = d6 / d5;
-/*     */
-/* 241 */       double d8 = pNPoint2.getPos() / (pNPoint2.getPos() + pNPoint2.getNeg()) + d7 * (d1 - pNPoint2.getPos()) / this.totPos;
-/*     */
-/*     */
-/* 244 */       double d9 = 0.5D * d2 * (d8 - d3);
-/*     */
-/* 246 */       d4 += d9;
-/*     */     }
-/* 248 */     d2 = pNPoint1.getPos() / this.totPos;
-/*     */
-/*     */
-/* 251 */     for (int i = b + 1; i < size(); i++) {
-/* 252 */       PNPoint pNPoint = elementAt(i);
-/*     */
-/* 254 */       double d5 = pNPoint.getPos() / this.totPos;
-/* 255 */       double d6 = pNPoint.getPos() / (pNPoint.getPos() + pNPoint.getNeg());
-/* 256 */       double d7 = (d5 - d2) * d6;
-/* 257 */       double d8 = 0.5D * (d5 - d2) * (d3 - d6);
-/*     */
-/*     */
-/* 260 */       d4 += d7 + d8;
-/* 261 */
-                d2 = d5;
-/* 263 */       d3 = d6;
-/*     */     }
-/*     */
-/* 267 */     return d4;
-/*     */   }
-/*     */
-/*     */
-/*     */
-/*     */
-/*     */
-/*     */
-/*     */   public double calculateAUCROC() {
-/*     */
-/*     */
-/* 281 */     if (size() == 0) {
-/* 282 */       System.err.println("ERROR: No data to calculate....");
-/* 283 */       return 0.0D;
-/*     */     }
-/*     */
-/* 286 */     PNPoint pNPoint = elementAt(0);
-/* 287 */     double d1 = pNPoint.getPos() / this.totPos;
-/* 288 */     double d2 = pNPoint.getNeg() / this.totNeg;
-/* 289 */     double d3 = 0.5D * d1 * d2;
-/*     */
-/*     */
-/*     */
-/* 293 */     for (byte b = 1; b < size(); b++) {
-/* 294 */       PNPoint pNPoint1 = elementAt(b);
-/* 295 */       double d4 = pNPoint1.getPos() / this.totPos;
-/* 296 */       double d5 = pNPoint1.getNeg() / this.totNeg;
-/* 297 */       double d6 = (d4 - d1) * d5;
-/* 298 */       double d7 = 0.5D * (d4 - d1) * (d5 - d2);
-/* 299 */       d3 += d6 - d7;
-/*     */
-/* 301 */
-                d1 = d4;
-/* 303 */       d2 = d5;
-/*     */     }
-/* 305 */     d3 = 1.0D - d3;
-/* 307 */     return d3;
-/*     */   }
-/*     */
-/*     */
+public class Confusion extends Vector<PNPoint>
+{
+  private final double totPos;
+  private final double totNeg;
 
-    public String toString() {
-        StringBuilder str = new StringBuilder("TotalPos: " + this.totPos + ", TotalNeg: " + this.totNeg + "\n");
-        for (byte b = 0; b < size(); b++) {
-            str.append(elementAt(b)).append("\n");
-        }
-        return str.toString();
+  public Confusion(double paramDouble1, double paramDouble2)
+  {
+     if (paramDouble1 < 1.0D || paramDouble2 < 1.0D) {
+       this.totPos = 1.0D;
+       this.totNeg = 1.0D;
+       System.err.println("ERROR: " + paramDouble1 + "," + paramDouble2 + " - " + "Defaulting Confusion to 1,1");
+    } else {
+       this.totPos = paramDouble1;
+       this.totNeg = paramDouble2;
+    }
+  }
+
+  public void addPoint(double paramDouble1, double paramDouble2) throws NumberFormatException
+  {
+     if (paramDouble1 < 0.0D || paramDouble1 > this.totPos || paramDouble2 < 0.0D || paramDouble2 > this.totNeg) {
+       throw new NumberFormatException();
+     }
+
+     PNPoint pNPoint = new PNPoint(paramDouble1, paramDouble2);
+     if (!contains(pNPoint)) {
+       add(pNPoint);
+    }
+  }
+
+  public void sort()
+  {
+    if (size() == 0) {
+      System.err.println("ERROR: No data to sort....");
+      return;
     }
 
+    PNPoint[] arrayOfPNPoint = new PNPoint[size()];
+    byte b1 = 0;
+    while (size() > 0) {
+      arrayOfPNPoint[b1++] = elementAt(0);
+      removeElementAt(0);
+    }
 
+    Arrays.sort(arrayOfPNPoint);
+    this.addAll(Arrays.asList(arrayOfPNPoint));
+
+    PNPoint pNPoint1 = elementAt(0);
+    while (pNPoint1.getPos() < 0.001D && pNPoint1.getPos() > -0.001D) {
+      removeElementAt(0);
+      pNPoint1 = elementAt(0);
+    }
+
+    double d = pNPoint1.getNeg() / pNPoint1.getPos();
+
+    PNPoint pNPoint2 = new PNPoint(1.0D, d);
+    if (!contains(pNPoint2) && pNPoint1.getPos() > 1.0D) {
+      insertElementAt(pNPoint2, 0);
+    }
+
+    pNPoint2 = new PNPoint(this.totPos, this.totNeg);
+    if (!contains(pNPoint2)) {
+      add(pNPoint2);
+    }
+  }
+
+  public void interpolate()
+  {
+    if (size() == 0) {
+      System.err.println("ERROR: No data to interpolate....");
+      return;
+    }
+
+    for (byte b = 0; b < size() - 1; b++) {
+      PNPoint pNPoint1 = elementAt(b);
+      PNPoint pNPoint2 = elementAt(b + 1);
+
+      double d1 = pNPoint2.getPos() - pNPoint1.getPos();
+      double d2 = pNPoint2.getNeg() - pNPoint1.getNeg();
+      double d3 = d2 / d1;
+      double d4 = pNPoint1.getPos();
+      double d5 = pNPoint1.getNeg();
+
+      while (Math.abs(pNPoint1.getPos() - pNPoint2.getPos()) > 1.001D) {
+        double d = d5 + (pNPoint1.getPos() - d4 + 1.0D) * d3;
+        PNPoint pNPoint = new PNPoint(pNPoint1.getPos() + 1.0D, d);
+        insertElementAt(pNPoint, ++b);
+        pNPoint1 = pNPoint;
+      }
+    }
+  }
+
+  public double calculateAUCPR(double paramDouble)
+  {
+    if (paramDouble < 0.0D || paramDouble > 1.0D) {
+      System.err.println("ERROR: invalid minRecall, must be between 0 and 1 - returning 0");
+      return 0.0D;
+    }
+
+    if (size() == 0) {
+      System.err.println("ERROR: No data to calculate....");
+      return 0.0D;
+    }
+
+    double d1 = paramDouble * this.totPos;
+    byte b = 0;
+    PNPoint pNPoint1 = elementAt(b);
+    PNPoint pNPoint2 = null;
+
+    try {
+      while (pNPoint1.getPos() < d1) {
+        pNPoint2 = pNPoint1;
+        pNPoint1 = elementAt(++b);
+      }
+    } catch (ArrayIndexOutOfBoundsException arrayIndexOutOfBoundsException) {
+      System.out.println("ERROR: minRecall out of bounds - exiting...");
+      System.exit(-1);
+    }
+
+    double d2 = (pNPoint1.getPos() - d1) / this.totPos;
+    double d3 = pNPoint1.getPos() / (pNPoint1.getPos() + pNPoint1.getNeg());
+    double d4 = d2 * d3;
+
+    if (pNPoint2 != null) {
+      double d5 = pNPoint1.getPos() / this.totPos - pNPoint2.getPos() / this.totPos;
+      double d6 = pNPoint1.getPos() / (pNPoint1.getPos() + pNPoint1.getNeg()) - pNPoint2.getPos() / (pNPoint2.getPos() + pNPoint2.getNeg());
+      double d7 = d6 / d5;
+      double d8 = pNPoint2.getPos() / (pNPoint2.getPos() + pNPoint2.getNeg()) + d7 * (d1 - pNPoint2.getPos()) / this.totPos;
+      double d9 = 0.5D * d2 * (d8 - d3);
+      d4 += d9;
+    }
+
+    d2 = pNPoint1.getPos() / this.totPos;
+    for (int i = b + 1; i < size(); i++) {
+      PNPoint pNPoint = elementAt(i);
+      double d5 = pNPoint.getPos() / this.totPos;
+      double d6 = pNPoint.getPos() / (pNPoint.getPos() + pNPoint.getNeg());
+      double d7 = (d5 - d2) * d6;
+      double d8 = 0.5D * (d5 - d2) * (d3 - d6);
+      d4 += d7 + d8;
+      d2 = d5;
+      d3 = d6;
+    }
+    return d4;
+  }
+
+  public double calculateAUCROC()
+  {
+    if (size() == 0) {
+      System.err.println("ERROR: No data to calculate....");
+      return 0.0D;
+    }
+    PNPoint pNPoint = elementAt(0);
+    double d1 = pNPoint.getPos() / this.totPos;
+    double d2 = pNPoint.getNeg() / this.totNeg;
+    double d3 = 0.5D * d1 * d2;
+
+    for (byte b = 1; b < size(); b++) {
+      PNPoint pNPoint1 = elementAt(b);
+      double d4 = pNPoint1.getPos() / this.totPos;
+      double d5 = pNPoint1.getNeg() / this.totNeg;
+      double d6 = (d4 - d1) * d5;
+      double d7 = 0.5D * (d4 - d1) * (d5 - d2);
+
+      d3 += d6 - d7;
+      d1 = d4;
+      d2 = d5;
+    }
+    d3 = 1.0D - d3;
+    return d3;
+  }
+
+  public String toString()
+  {
+    StringBuilder str = new StringBuilder("TotalPos: " + this.totPos + ", TotalNeg: " + this.totNeg + "\n");
+    for (byte b = 0; b < size(); b++) {
+      str.append(elementAt(b)).append("\n");
+    }
+    return str.toString();
+  }
 }
