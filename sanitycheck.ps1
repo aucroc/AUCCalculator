0affff21daf1767fe006799f51410f86cd35d534
Alexander L. Hayes <alexander@batflyer.net>
2021-11-14 21:10:10 +0000

    🔥 Drop sanitycheck scripts

--- sanitycheck.ps1
+++
@@ -1,5 +0,0 @@
-mvn clean
-mvn package
-java -jar .\target\auc-0.2.1-jar-with-dependencies.jar .\testsetlist.txt list 0.0
-java -jar .\target\auc-0.2.1-jar-with-dependencies.jar .\testsetlist.txt list 0.5
-java -jar .\target\auc-0.2.1-jar-with-dependencies.jar .\testsetlist.txt list 1.0
--- sanitycheck.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-mvn clean
-mvn package
-java -jar target/auc-0.2.1-jar-with-dependencies.jar testsetlist.txt list 0.0
-java -jar target/auc-0.2.1-jar-with-dependencies.jar testsetlist.txt list 0.5
-java -jar target/auc-0.2.1-jar-with-dependencies.jar testsetlist.txt list 1.0
