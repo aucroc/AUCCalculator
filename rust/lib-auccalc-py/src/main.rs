98ca3c2cf0437666477569f5127be362e4724470
Alexander L. Hayes <alexander@batflyer.net>
2021-12-23 09:38:35 -0500

    Refactor split Rust and Python into two crates

---
+++ rust/lib-auccalc-py/src/main.rs
@@ -0,0 +1,11 @@
+// Copyright © 2021 Alexander L. Hayes
+// MIT License
+
+use lib_auccalc::add_two;
+
+fn main() {
+
+    println!("2 + 2 = {}", add_two(2, 2));
+
+    println!("{}", "Hello World.");
+}
