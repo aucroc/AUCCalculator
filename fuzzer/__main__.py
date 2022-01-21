1cb797e281ea6ea5719cced1e23c465d20d76b82
hayesall <alexander@batflyer.net>
2022-01-20 20:10:15 -0500

    ✅ Add `ConfusionPy` to `fuzzer`

--- fuzzer/__main__.py
+++ fuzzer/__main__.py
@@ -1,6 +1,7 @@
 # Copyright © 2021 Alexander L. Hayes
 # MIT License
 
+from auccalc.auccalc import ConfusionPy
 from .generate import generate_output_file
 import subprocess
 import sys
@@ -15,10 +16,18 @@ def fuzz():
     i = 0
     while True:
 
-        generate_output_file(i + 5)
+        probs, labels = generate_output_file(i + 5)
 
         out0 = run_jar_file("history/original-auc.jar", 0.0)
         out1 = run_jar_file("build/libs/auc-0.3.0.jar", 0.0)
+
+        out0_utf8 = out0.stdout.decode("utf-8").splitlines()[-2:]
+        out0_aucpr = float(out0_utf8[0].split(" ")[-1])
+        out0_aucro = float(out0_utf8[1].split(" ")[-1])
+
+        conf = ConfusionPy(probs, labels)
+        out2_aucpr = conf.aucpr(0.0)
+        out2_aucro = conf.aucroc()
 
         if out0.stdout != out1.stdout:
             print(f"stdout differed at iteration, {i}")
@@ -28,9 +37,16 @@ def fuzz():
             print(f"stderr differed at iteration, {i}")
             sys.exit(0)
 
+        if out0_aucpr != out2_aucpr:
+            print(f"AUCPR differed at iteration, {i}")
+            sys.exit(0)
+
+        if out0_aucro != out2_aucro:
+            print(f"AUCROC differed at iteration, {i}")
+            sys.exit(0)
+
         print(f"{i} fuzzing iterations", end="\r")
         i += 1
-
 
 if __name__ == "__main__":
     fuzz()
